78aba09bee597cf8c60f61aa1e24c1bb8e62c96e
Allen <32953986+ALannister@users.noreply.github.com>
2020-06-27 00:43:46 +0800

    docs: 添加.gitignore文件

---
+++ frauddetection/src/main/java/spendreport/FraudDetectionJob.java
@@ -0,0 +1,7 @@
+package spendreport;
+
+/**
+ * Created by Lannister on 2020/6/25.
+ */
+public class FraudDetectionJob {
+}
---
+++ spendreport/src/main/java/spendreport/SpendReport.java
@@ -0,0 +1,7 @@
+package spendreport;
+
+/**
+ * Created by Lannister on 2020/6/27.
+ */
+public class SpendReport {
+}
